f37971c2e160e50841ccef662375fbd514f954a6
itsu020402 <tsubaki394>
2020-02-24 12:18:01 +0900

    アプリ購入処理の実装

--- src/main/java/dev/itsu/cphoneappmarket/AppDetailsActivity.kt
+++ src/main/java/dev/itsu/cphoneappmarket/AppDetailsActivity.kt
@@ -8,6 +8,7 @@ import net.comorevi.cphone.cphone.model.Response
 import net.comorevi.cphone.cphone.sql.ApplicationSQLManager
 import net.comorevi.cphone.cphone.widget.activity.ReturnType
 import net.comorevi.cphone.cphone.widget.activity.base.ModalActivity
+import net.comorevi.moneyapi.MoneySAPI
 
 class AppDetailsActivity(manifest: ApplicationManifest, val app: ApplicationManifest) : ModalActivity(manifest) {
 
@@ -18,7 +19,7 @@ class AppDetailsActivity(manifest: ApplicationManifest, val app: ApplicationMani
         val modalResponse = response as ModalResponse
         return when {
             modalResponse.isButton1Clicked -> {
-                ConfirmBuyingAppActivity(manifest, hasApp).start(bundle)
+                ConfirmBuyingAppActivity(manifest, app, hasApp).start(bundle)
                 ReturnType.TYPE_CONTINUE
             }
             modalResponse.isButton2Clicked -> ReturnType.TYPE_END
@@ -32,12 +33,12 @@ class AppDetailsActivity(manifest: ApplicationManifest, val app: ApplicationMani
 
         this.title = app.title
         this.content = """
-            ${bundle.getString("ad_name")} ${app.title} ${TextFormat.RESET}
+            §a${app.title} ${TextFormat.RESET}
             ${if (hasApp) bundle.getString("sr_installed") else bundle.getString("sr_not_installed")} ${TextFormat.RESET}
             
             ${app.description} ${TextFormat.RESET}
             
-            ${bundle.getString("ad_price")} ${app.price} ${TextFormat.RESET}
+            ${bundle.getString("ad_price")} ${app.price}${MoneySAPI.getInstance().moneyUnit} ${TextFormat.RESET}
             ${bundle.getString("ad_version")} ${app.version}
             ${bundle.getString("ad_author")} ${app.author}
         """.trimIndent()
--- src/main/java/dev/itsu/cphoneappmarket/ConfirmBuyingAppActivity.kt
+++ src/main/java/dev/itsu/cphoneappmarket/ConfirmBuyingAppActivity.kt
@@ -1,23 +1,70 @@
 package dev.itsu.cphoneappmarket
 
+import cn.nukkit.utils.TextFormat
 import net.comorevi.cphone.cphone.application.ApplicationManifest
+import net.comorevi.cphone.cphone.application.ApplicationPermission
 import net.comorevi.cphone.cphone.model.Bundle
+import net.comorevi.cphone.cphone.model.ModalResponse
 import net.comorevi.cphone.cphone.model.Response
+import net.comorevi.cphone.cphone.sql.ApplicationSQLManager
 import net.comorevi.cphone.cphone.widget.activity.ReturnType
 import net.comorevi.cphone.cphone.widget.activity.base.ModalActivity
+import net.comorevi.moneyapi.MoneySAPI
 
-class ConfirmBuyingAppActivity(manifest: ApplicationManifest, private val hasApp: Boolean) : ModalActivity(manifest) {
+class ConfirmBuyingAppActivity(manifest: ApplicationManifest, private val app: ApplicationManifest, private val hasApp: Boolean) : ModalActivity(manifest) {
 
     private lateinit var bundle: Bundle
 
     override fun onStop(response: Response): ReturnType {
-        // TODO install and uninstall app
-        bundle.cPhone.homeMessage = bundle.getString("installed_app")
+        val modalResponse = response as ModalResponse
+        if (modalResponse.isButton2Clicked || !modalResponse.isButton1Clicked) return ReturnType.TYPE_END
+
+        if (hasApp) {
+            ApplicationSQLManager.unInstallApplication(bundle.cPhone.player.name, app)
+            bundle.cPhone.homeMessage = bundle.getString("uninstalled_app") + " (${app.title})"
+            return ReturnType.TYPE_END
+        }
+
+        val money = MoneySAPI.getInstance().getMoney(bundle.cPhone.player)
+        val isEnough = money >= app.price
+
+        if (!isEnough) {
+            bundle.cPhone.homeMessage = bundle.getString("cb_error_money")
+            return ReturnType.TYPE_END
+        }
+
+        ApplicationSQLManager.installApplication(bundle.cPhone.player.name, app)
+        MoneySAPI.getInstance().reduceMoney(bundle.cPhone.player.name, app.price)
+        bundle.cPhone.homeMessage = bundle.getString("installed_app") + " (${app.title})"
+
         return ReturnType.TYPE_END
     }
 
     override fun onCreate(bundle: Bundle) {
         this.bundle = bundle
+
+        val money = MoneySAPI.getInstance().getMoney(bundle.cPhone.player)
+        val isEnough = money >= app.price
+
+        title = bundle.getString("cb_title")
+        content = if (hasApp) bundle.getString("cb_uninstall") else {
+            """
+                ${bundle.getString("cb_install")}
+                
+                ${bundle.getString("ad_price")} ${app.price}${MoneySAPI.getInstance().moneyUnit} ${TextFormat.RESET}
+                ${bundle.getString("my_money")} ${if (isEnough) "§a" else "§c"} $money${MoneySAPI.getInstance().moneyUnit} ${MoneySAPI.getInstance().moneyUnit} ${TextFormat.RESET}
+            """.trimIndent()
+
+        }
+
+        button1Text = when {
+            app.permission == ApplicationPermission.ATTRIBUTE_DEFAULT -> bundle.getString("back_to_top")
+            hasApp -> bundle.getString("uninstall")
+            !isEnough -> "§c" + bundle.getString("install")
+            else -> bundle.getString("install")
+        }
+
+        button1Text = bundle.getString("back_to_top")
     }
 
 }
